e9b8a6873a60e7bbd3b6e14d0a00c435fbbea64c
Juan <juan.zapata19@udea.edu.co>
2018-03-07 16:34:46 -0500

    Segundo

---
+++ dojo-spring/src/main/java/co/edu/udea/dojospring/DojoSpringApplication.java
@@ -0,0 +1,12 @@
+package co.edu.udea.dojospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoSpringApplication.class, args);
+	}
+}
